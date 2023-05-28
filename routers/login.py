bbed0d160f9bc45cce8b90b78c280b7cbfa15e8e
junhyeong.kim <junhyeong.kim@brainpad.co.jp>
2023-05-28 21:54:25 +0900

    remove unused code

--- routers/login.py
+++ routers/login.py
@@ -49,25 +49,3 @@ async def login_for_access_token(
 
     # return {"access_token": access_token, "token_type": "bearer"}
     return response
-
-
-# @app.get("/access_token")
-# async def read_users_me(request: Request):
-#     access_token: str = request.cookies.get("access_token")
-#     _, access_token = access_token.split(" ")
-#     current_user = await get_current_user(token=access_token)
-#     return current_user
-
-
-# @app.get("/users/me/", response_model=User)
-# async def read_users_me(
-#     current_user: Annotated[User, Depends(get_current_active_user)]
-# ):
-#     return current_user
-
-
-# @app.get("/users/me/items/")
-# async def read_own_items(
-#     current_user: Annotated[User, Depends(get_current_active_user)]
-# ):
-#     return [{"item_id": "Foo", "owner": current_user.username}]
