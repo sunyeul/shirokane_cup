97d9480366febb46fec61d69205dc643b3a7d490
junhyeong.kim <junhyeong.kim@brainpad.co.jp>
2023-06-02 12:48:23 +0900

    show competitions referring competitions table

--- routers/competitions.py
+++ routers/competitions.py
@@ -2,10 +2,9 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from jinja2 import FileSystemLoader
+from database import get_db
+from models import Competition
 
-import os
-
-from importlib import import_module
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -19,12 +18,13 @@ templates.env.loader = FileSystemLoader(["./templates", "./competitions"])
 async def top_page(request: Request):
     competition_links = []
 
-    for compe in os.listdir("./competitions"):
-        competition_link = {}
-        competition_link["href"] = f"/competitions/{compe}/overview"
+    db = next(get_db())
+    competitions = db.query(Competition).all()
 
-        compe_module = import_module(f"competitions.{compe}")
-        competition_link["name"] = compe_module.competition_title
+    for compe in competitions:
+        competition_link = {}
+        competition_link["href"] = f"/competitions/{compe.name}/overview"
+        competition_link["title"] = compe.title
 
         competition_links.append(competition_link)
 
